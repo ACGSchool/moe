a0fad33c3e622dcb97e661fe04e177fa606e26d7
稲森光香 <42112059+itoukou1@users.noreply.github.com>
2023-05-25 10:49:18 +0800

    受验需求更新

--- school-loader.js
+++ school-loader.js
@@ -6,7 +6,7 @@ fetch("schools.json?" + String(Date.parse(new Date()))).then(res => {
     let table = document.createElement("table");
     table.classList.add("table", "table-hover");
     let thead = document.createElement("thead");
-    thead.innerHTML = `<tr><th scope="col" style="min-width: 170px;">学校名</th><th scope="col" style="min-width: 30px;">成立时间</th><th scope="col" style="min-width: 270px;">申请条件</th><th scope="col" style="min-width: 170px;">学校类型</th><th scope="col">网站</th></tr>`;
+    thead.innerHTML = `<tr><th scope="col" style="min-width: 170px;">学校名</th><th scope="col" style="min-width: 30px;">成立时间</th><th scope="col" style="min-width: 270px;">申请条件</th><th scope="col" style="min-width: 170px;">学校类型</th><th scope="col">网站</th><th scope="col" style="min-width: 70px;">受验需求</th></tr>`;
     table.appendChild(thead);
     let tbody = document.createElement("tbody");
     for (let i in data) {
@@ -23,6 +23,13 @@ fetch("schools.json?" + String(Date.parse(new Date()))).then(res => {
                 tr.appendChild(td);
             }
         }
+        let td = document.createElement("td");
+        if (data[i]['questionnaire']!==undefined){
+            td.innerHTML = " <span style='color: darkgreen;'><svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 24 24\" style='    fill: currentColor;'><path d=\"M12 2C6.5 2 2 6.5 2 12S6.5 22 12 22 22 17.5 22 12 17.5 2 12 2M10 17L5 12L6.41 10.59L10 14.17L17.59 6.58L19 8L10 17Z\"></path></svg></span>"
+        }else{
+            td.innerHTML = " <span style='color: red;'><svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 24 24\" style='fill: currentColor;'><path d=\"M12,2C17.53,2 22,6.47 22,12C22,17.53 17.53,22 12,22C6.47,22 2,17.53 2,12C2,6.47 6.47,2 12,2M15.59,7L12,10.59L8.41,7L7,8.41L10.59,12L7,15.59L8.41,17L12,13.41L15.59,17L17,15.59L13.41,12L17,8.41L15.59,7Z\"></path></svg></span>"
+        }
+        tr.appendChild(td)
         tbody.append(tr);
     }
     table.appendChild(tbody);
